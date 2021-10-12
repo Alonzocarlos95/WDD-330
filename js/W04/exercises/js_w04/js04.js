a0f15b8625c3b60d9bc4658063fc71aa1482f882
Carlos Alonzo <carlosalonzo95@hotmail.com>
2021-10-12 01:58:32 -0500

    Week04

---
+++ js/W04/exercises/js_w04/js04.js
@@ -0,0 +1,12 @@
+// const myName = document.getElementById('name');
+// myName.addEventListener('focus',() => {
+//     if(myName.value === 'Jhon'){
+//         myName.value = '';
+//     }
+// },false);
+
+// myName.addEventListener('blur',() => {
+//     if(myName.value === ''){
+//         myName.value = "Jhon";
+//     }
+// },false);
